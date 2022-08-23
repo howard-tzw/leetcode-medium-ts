1c581c2a76e8b1741df345abedc862ca32f0c93c
Howard <howard.tzw@gmail.com>
2022-08-23 09:14:33 +0800

    add: 1583

---
+++ 1583_reduce_array_size_to_the_half.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/reduce-array-size-to-the-half/
+
+function minSetSize(arr: number[]): number {
+    return 0
+};
+
+
+const main = ()=> {
+    const  arr = [3,3,3,3,5,5,5,2,2,7]
+    console.log(minSetSize(arr))
+}
+main()
